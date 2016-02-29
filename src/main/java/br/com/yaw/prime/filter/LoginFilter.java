31214a423ab864c4c312bd6aa44c4af681e6dfe8
FredBob <sensesoluti@gmail.com>
2016-02-29 15:54:41 -0300

    LoginFilter.xml

--- src/main/java/br/com/yaw/prime/filter/LoginFilter.java
+++ src/main/java/br/com/yaw/prime/filter/LoginFilter.java
@@ -49,14 +49,14 @@ public class LoginFilter implements Filter {
 			request.setAttribute(FILTER_APPLIED, Boolean.TRUE);
 			
 			if ((user != null)) {
-				hresp.sendRedirect("/supermercado_localhost/faces/filtrarMercadorias.xhtml");
+				hresp.sendRedirect("/supermercado/faces/filtrarMercadorias.xhtml");
 				return;
 			}
 		}else if((request.getAttribute(FILTER_APPLIED) == null) && paginaAtual != null) {
 			request.setAttribute(FILTER_APPLIED, Boolean.TRUE);
 			
 			if ((user == null)) {
-				hresp.sendRedirect("/supermercado_localhost/faces/login.xhtml");
+				hresp.sendRedirect("/supermercado/faces/login.xhtml");
 				return;
 			}
 		}
